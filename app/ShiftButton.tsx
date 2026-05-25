7a43e778c797e27273d2d4ca4e11b1ac91979ee7
Rohan Kumar <rohan@Rohans-MacBook-Air.local>
2026-05-25 20:35:09 +0530

    style: shift purple arrow container box lower (y: 18) below button

--- app/ShiftButton.tsx
+++ app/ShiftButton.tsx
@@ -102,8 +102,8 @@ export default function ShiftButton({
       {showIcon && (
         <motion.div
           variants={{
-            initial: { width: 0, opacity: 0, scale: 0.5, y: 12 },
-            hover: { width: baseHeight, opacity: 1, scale: 1, y: 12 }
+            initial: { width: 0, opacity: 0, scale: 0.5, y: 18 },
+            hover: { width: baseHeight, opacity: 1, scale: 1, y: 18 }
           }}
           transition={springTransition}
           style={{
