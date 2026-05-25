a81749b3a80da2232f3e5067570d5e2dcb9bef70
Rohan Kumar <rohan@Rohans-MacBook-Air.local>
2026-05-25 20:25:20 +0530

    style: make both Left and Right Icon arrows diagonal in ShiftButton

--- app/ShiftButton.tsx
+++ app/ShiftButton.tsx
@@ -113,8 +113,8 @@ export default function ShiftButton({
           }}
         >
           <svg width={baseHeight * 0.4} height={baseHeight * 0.4} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
-            <line x1="5" y1="12" x2="19" y2="12"></line>
-            <polyline points="12 5 19 12 12 19"></polyline>
+            <line x1="7" y1="17" x2="17" y2="7"></line>
+            <polyline points="7 7 17 7 17 17"></polyline>
           </svg>
         </motion.div>
       )}
